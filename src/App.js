8fb45171f3fb7cd4d234a036dd3e432d0f0d5244
Jonathan Montoya <montoya0396@gmail.com>
2020-03-30 17:54:15 -0400

    used james' css styling and fixed errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, {useState} from 'react';
-<<<<<<< HEAD
 import './inputpad.css';
 import DesignContainer from './components/DesignContainer';
 
 
-=======
-import './App.css';
-
-import Container from './components/Container';
->>>>>>> 2ee1d6fb5188dec59c7ad0803f7ae2d58bf078ae
 
 
 function App() {
@@ -16,11 +10,7 @@ function App() {
 
   return (
     <div>
-<<<<<<< HEAD
       <DesignContainer />
-=======
-      <Container />
->>>>>>> 2ee1d6fb5188dec59c7ad0803f7ae2d58bf078ae
     </div>
   );
 }
--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, {useState} from 'react';
 import DesignBoard from './DesignBoard';
 import InputBoard from './InputBoard';
@@ -49,45 +48,3 @@ render()
   
   export default DesignContainer;
 
-=======
-import React, {useState} from 'react'
-import DesignBoard from './DesignBoard'
-import InputPad from './InputPad'
-import { render } from '@testing-library/react'
-
-
-class DesignContainer extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-             isColor: ["white", "red"],
-             showBoard: true,
-             board: true,
-            
-        }
-        this.setState({color: 'red', input: 1})
-    }
-    
-
-
-render(){
-    return(
-        <div>
-        {/* <button onClick={()=>this.isColor}>Change color</button> */}
-            <div isColor={this.isColor}></div>
-            {/* <div className="board" style={{background: this.state.color}}></div> */}
-            <div className="inputPad"></div>
-            <InputPad />
-            <DesignBoard />
-        </div>
-        )
-    }
-
-}
-
-
-   
-
-
-export default DesignContainer
->>>>>>> 2ee1d6fb5188dec59c7ad0803f7ae2d58bf078ae
